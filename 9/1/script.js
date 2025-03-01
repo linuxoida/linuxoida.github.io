7c75773f04421cfefc310c6ba4f9008f4e722f19
linuxoida <ks-x40.laptev.ns@urtt.ru>
2025-03-02 01:13:32 +0500

    new file:   9/1 v1/index.html 	renamed:    "9/1/script \342\200\224 \320\272\320\276\320\277\320\270\321\217.js" -> "9/1 v1/script \342\200\224 \320\272\320\276\320\277\320\270\321\217.js" 	new file:   9/1 v1/script.js 	renamed:    9/1/style.css -> 9/1 v1/style.css 	new file:   9/1 v1/words.js 	modified:   9/1/index.html 	modified:   9/1/script.js 	modified:   style.css

--- 9/1/script.js
+++ 9/1/script.js
@@ -7,12 +7,12 @@ for (let i = 1; i <= 100; i++) {
 exerciseAmountString += '</select> <input type="button" onclick = "exercise()" value="✔" /></form>';
 
 
-document.getElementById('exerciseAmount').innerHTML += exerciseAmountString
-//document.getElementById('exerciseAmount').innerHTML += '<input type="button" onclick = "exercise()" value="⇄ Сгенерировать" />'
+//document.getElementById('exerciseAmount').innerHTML += exerciseAmountString
+$("#exerciseAmount").html(exerciseAmountString);
 function exerciseTypeOne(Num) {
 	const bits = [8,16,32]
 	const kolvoBit = bits[Math.floor(Math.random() * 3)]
-	let outputText ='<strong>' + (Num+1) + '.</strong> В одной из кодировок каждый символ кодируется <strong>' + kolvoBit + '</strong> битами.<br>Ученик записал текст: <br><br>"Города '
+	let outputText ='<strong>' + (Num+1) + '.</strong> В одной из кодировок каждый символ кодируется ' + kolvoBit + ' битами.<br>Ученик записал текст: <br><br>"Города '
 	let temp = Math.floor(Math.random() * country.length)
 	outputText += country[temp] + ": "
 	for(let i = 0; i < words[temp].length; i++) {
@@ -24,10 +24,10 @@ function exerciseTypeOne(Num) {
 			outputText += '"<br>'
 		}
 	}
-	outputText += '<br>После этого ученик убрал из текста один из городов, а также ставшие лишними запятую и пробел. После этого размер предложения оказался на <strong>'
+	outputText += '<br>После этого ученик убрал из текста один из городов, а также ставшие лишними запятую и пробел. После этого размер предложения оказался на '
 	const outputCorrectAnswer = words[temp][Math.floor(Math.random() * words[temp].length)]
 	let kolvoBytes = (outputCorrectAnswer.length + 2) * kolvoBit / 8 
-	outputText += kolvoBytes + '</strong>' 
+	outputText += kolvoBytes;
 	if (kolvoBytes%10 == 1 && kolvoBytes%100 != 11) {
 		outputText += ' байт';
 	}
@@ -43,21 +43,24 @@ function exerciseTypeOne(Num) {
 function exerciseTypeTwo(Num) {
 	const bits = [8,16,32];
 	const kolvoBit = bits[Math.floor(Math.random() * 3)];
-	let outputText ='<strong>' + (Num+1) + '.</strong> В одной из кодировок каждый символ кодируется <strong>' + kolvoBit + '</strong> битами. Определите размер в байтах следующего предложения в данной кодировке, не учитывая кавычки: ';
+	let outputText ='<strong>' + (Num+1) + '.</strong> В одной из кодировок каждый символ кодируется ' + kolvoBit + ' битами. Определите размер в байтах следующего предложения в данной кодировке, не учитывая кавычки: ';
 	const Sentence = sentences[Math.floor(Math.random() * sentences.length)];
-	outputText += '"<strong>' + Sentence + '</strong>"';
+	outputText += '"' + Sentence + '"';
 	const outputCorrectAnswer = (Sentence.length * kolvoBit / 8).toString();
 	return [outputText,outputCorrectAnswer];
 }
 let Amount = 0;
 function exercise () {
 	Amount = document.getElementById('exerciseAmountNum').value
-	document.getElementById('exerciseAmount').innerHTML = ''
+	//Amount = $("#exerciseAmountNum").attr("value");
+	document.getElementById('exerciseAmount').innerHTML = '';
 	for (let Num = 0; Num < Amount; Num++) {
-		document.getElementById('exercise1').innerHTML += '<div id = "example' + Num + '"></div><br><div class="form-example"><input type="text" name="name" id="otvetPole' + Num + '" required /><input type="button" id = "knopka' + Num +'" onclick = "attempt(' + Num + ')" value="Проверить результат" /></div><div id = "otvet' + Num + '"></div><h2></h2>'
-			
+		document.getElementById('exercise1').innerHTML += '<div  style = "margin-top: 1em;" id = "example' + Num + '"></div><br><div class="form-example"><input type="text" name="name" id="otvetPole' + Num + '" required /><input type="button" id = "knopka' + Num +'" onclick = "attempt(' + Num + ')" value="Проверить результат" /></div><div id = "otvet' + Num + '"></div><div style="border: 1px solid #aaa; margin-top: 1em;"></div>'
+		//$("#exercise1").append('<div id = "example' + Num + '"></div><br><div class="form-example"><input type="text" name="name" id="otvetPole' + Num + '" required /><input type="button" id = "knopka' + Num +'" onclick = "attempt(' + Num + ')" value="Проверить результат" /></div><div id = "otvet' + Num + '"></div><h2></h2>');
 		document.getElementById('otvetPole' + Num ).value = ''
+		//$("#otvetPole").attr("value","");
 		document.getElementById('otvet' + Num ).innerHTML = ''
+		//$("#otvet").html("");
 		randomExercise = Math.floor(Math.random() * 4);
 		let output = '';
 		if (randomExercise == 0 || randomExercise == 1 || randomExercise == 2)  {
@@ -68,8 +71,8 @@ function exercise () {
 			
 		}
 		askWord[Num] = output[1]
-		document.getElementById('example' + Num ).innerHTML += output[0]
-		
+		//document.getElementById('example' + Num ).innerHTML += output[0]
+		$("#example" + Num).append(output[0]);
 	}
 	
 }
